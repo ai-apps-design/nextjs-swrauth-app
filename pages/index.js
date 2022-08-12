709652d2064e7143fff8c936f89aec4c7189d336
smartappcoder <dennis.sheu@gmail.com>
2022-08-12 15:50:21 -0700

    Add SWR SSR test.

--- pages/index.js
+++ pages/index.js
@@ -1,129 +1,42 @@
-// import { useEffect } from "react";
-// import Router from "next/router";
+import useSWR, { SWRConfig } from "swr";
 
-// import Nav from "../components/nav";
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const API = "https://api.github.com/repos/vercel/swr";
 
-// import useUser from "../data/use-user";
-// import { login } from "../libs/auth";
+export async function getServerSideProps() {
+  const repoInfo = await fetcher(API);
+  return {
+    props: {
+      fallback: {
+        [API]: repoInfo
+      }
+    }
+  };
+}
 
-// export default function App() {
-//   const { user, mutate, loggedOut } = useUser();
+function Repo() {
+  const { data, error } = useSWR(API);
 
-//   // if logged in, redirect to the dashboard
-//   useEffect(() => {
-//     if (user && !loggedOut) {
-//       Router.replace("/dashboard");
-//     }
-//   }, [user, loggedOut]);
+  // there should be no `undefined` state
+  console.log("Is data ready?", !!data);
 
-//   return (
-//     <div className="homepage">
-//       <Nav title="ACME" />
-//       <main>
-//         <h1>ACME!</h1>
-//         <p>Build Something Brilliant</p>
-//         <br />
-//         <button
-//           onClick={() => {
-//             login();
-//             mutate(); // after logging in, we revalidate the SWR
-//           }}
-//         >
-//           Login To Continue
-//         </button>
-//       </main>
-//     </div>
-//   );
-// }
-
-import { usePaginatePosts } from "/libs/useRequest";
-
-import Post from "/components/post";
-
-export default function IndexPage() {
-  const {
-    posts,
-    error,
-    isLoadingMore,
-    size,
-    setSize,
-    isReachingEnd
-  } = usePaginatePosts("/posts");
-
-  if (error) return <h1>Something went wrong!</h1>;
-  if (!posts) return <h1>Loading...</h1>;
-
+  if (error) return "An error has occurred.";
+  if (!data) return "Loading...";
   return (
-    <div className="container">
-      <h1>My Posts</h1>
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
-      <button
-        disabled={isLoadingMore || isReachingEnd}
-        onClick={() => setSize(size + 1)}
-      >
-        {isLoadingMore
-          ? "Loading..."
-          : isReachingEnd
-          ? "No more posts"
-          : "Load more"}
-      </button>
-      <style jsx global>
-        {`
-          @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap");
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
-          body {
-            font-family: "Nunito", sans-serif;
-            background: #222;
-            color: #fff;
-            font-size: 1rem;
-          }
-          a {
-            color: #fff;
-            text-align: center;
-          }
-          .container {
-            max-width: 728px;
-            margin: auto;
-            padding: 1rem;
-          }
-          .container > h1 {
-            text-align: center;
-            text-transform: uppercase;
-            margin-bottom: 1rem;
-            font-size: 1.4rem;
-          }
-          h1 {
-            text-transform: capitalize;
-            font-size: 1.1rem;
-          }
-          button {
-            display: block;
-            margin: auto;
-            padding: 0.5rem 1rem;
-            font-size: 1rem;
-            font-weight: 700;
-            background: #0dbbac;
-            color: #fff;
-            border-radius: 20px;
-            border: none;
-            cursor: pointer;
-          }
-          .Card {
-            background: #333;
-            padding: 1rem;
-            margin-bottom: 1rem;
-          }
-          .Card--body {
-            color: #999;
-          }
-        `}
-      </style>
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
     </div>
+  );
+}
+
+export default function App({ fallback }) {
+  return (
+    <SWRConfig value={{ fallback }}>
+      <Repo />
+    </SWRConfig>
   );
 }
