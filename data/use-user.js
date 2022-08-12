f29496586925e3bb76de5510c5351f5e9ef22689
smartappcoder <dennis.sheu@gmail.com>
2022-08-12 15:15:47 -0700

    SWR test on Vercel.

---
+++ data/use-user.js
@@ -0,0 +1,19 @@
+import useSWR from "swr";
+
+import userFetcher, { mock_user_api } from "../libs/api-user";
+
+export default function useUser() {
+  // const { data, mutate, error } = useSWR("api_user", userFetcher);
+  const { data, mutate, error } = useSWR("api_user", mock_user_api);
+
+  const loading = !data && !error;
+  const loggedOut = error && error.status === 403;
+
+  return {
+    loading,
+    loggedOut,
+    user: data,
+    mutate
+  };
+}
+
